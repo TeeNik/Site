568cc76941e9482f054e498bb27bcf3a1d9a62c8
teenik <teenik3315@gmail.com>
2017-05-25 11:06:24 +0300

    menu [2]

---
+++ scripts/menu.js
@@ -0,0 +1,4 @@
+(function)(){
+    return [{"Главная","https://teesite.herokuapp.com"},
+            {"О нас",""},{"GitHub","https://teesite.herokuapp.com"},{"Главная","https://teesite.herokuapp.com"},{"Главная","https://teesite.herokuapp.com"}]
+}
