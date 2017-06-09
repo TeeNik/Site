58da7912375fa7afdab1afc0dd379a254c1cd514
TeeNik <teenik3315@gmail.com>
2017-06-10 02:10:05 +0300

    menu close

--- public/scripts/manage.js
+++ public/scripts/manage.js
@@ -1,7 +1,11 @@
 (function(){
 	let header = document.getElementsByClassName("header")[0];
-	console.log(header);
 	header.addEventListener("click", function(){
 		document.location.href = "/../..";
 	});
+	
+	let mask = document.getElementById("mask-content");
+	mask.addEventListener('click', function(){
+		document.getElementById('nav-toggle').checked = false;
+	})	
 })();
